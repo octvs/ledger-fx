f4d0b4746f7e77a38733f0709965379b06922d16
octvs <octvs@posteo.de>
2026-06-09 16:17:17 +0200

    build: pull selectolax from upstream

--- flake.nix
+++ flake.nix
@@ -12,18 +12,6 @@
       systems = import inputs.systems;
       perSystem = {pkgs, ...}: let
         dependencies = with pkgs.python3.pkgs; [selectolax pandas httpx];
-        selectolax = pkgs.python3Packages.buildPythonPackage rec {
-          # TODO: Upstream
-          pname = "selectolax";
-          version = "0.3.28";
-          pyproject = true;
-          src = pkgs.python3Packages.fetchPypi {
-            inherit pname version;
-            sha256 = "iuPalyYbV77VH6mwPEiJhfbc/y4uREcaqfXiwXzBxFo=";
-          };
-          build-system = with pkgs.python3.pkgs; [setuptools];
-          doCheck = false;
-        };
       in {
         packages.default = pkgs.python3Packages.buildPythonApplication {
           pname = "ledger-fx";
